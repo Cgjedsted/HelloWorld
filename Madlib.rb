98bae4065b8ef4c47171d75ba56230731a050028
Charlotte Gjedsted <37193327+Cgjedsted@users.noreply.github.com>
2018-03-21 19:51:59 -0700

    Update Madlib.rb

--- Madlib.rb
+++ Madlib.rb
@@ -26,43 +26,8 @@ proper_noun = gets.chomp
 puts "Name a synonym for said."
 said_synonym = gets.chomp
 
-# use the input to create a madlib
-# Madlib I was on my way to (place)
-# I spotted a (adjective) by a (noun)
-# The (noun) made me (verb) with (feeling)
-# I felt is in my (body part)
-# Suddenly, a (adjective) (noun) appeared.
-# Someone call the (Proper Noun) I (synonym for said)
-
 # access input from user and output into console
 # for the output section, I used the repl.it which was demoed in the video from the lesson. This was helpful in testing the code.
-
-puts "Name a time of day: morning, noon, or evening"
-time_of_day = gets.chomp
-puts "Name a place"
-location = gets.chomp
-puts "Name an adjective."
-first_adjective = gets.chomp
-puts "Name a noun."
-first_noun = gets.chomp
-puts "Name a verb."
-first_verb = gets.chomp
-puts "Name a feeling."
-feeling = gets.chomp
-puts "Name a body part"
-body_part = gets.chomp
-puts "Name another adjective."
-second_adjective = gets.chomp
-puts "Name another noun."
-second_noun = gets.chomp
-puts "Name a final adjective."
-final_adjective = gets.chomp
-puts "Name a final noun."
-final_noun = gets.chomp
-puts "Name a proper noun."
-proper_noun = gets.chomp
-puts "Name a synonym for said."
-said_synonym = gets.chomp
 
 # Madlib I was on my way to (place)
 # I spotted a (adjective) by a (noun)
