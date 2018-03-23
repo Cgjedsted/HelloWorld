c2e80de1e838957b12ab49af2d500ec6935d6fdb
Charlotte Gjedsted <37193327+Cgjedsted@users.noreply.github.com>
2018-03-22 21:47:08 -0700

    Update using other Ada Jumpstart students
    Tatsqui, I can't figure out how to leave a comment or message you, but your code was super helpful. It helped me realize to define options, create a conditional flow for candy choices to assign names and prices for the computer, and to create a conditional flow for calculating prices (including if the option was not one offered). Thank you.

--- Candy Machine Assignment.rb
+++ Candy Machine Assignment.rb
@@ -1,46 +1,59 @@
+def options
+  "A Twix: $0.75
+  B M and Ms: $0.50
+  C Reeses Peanut Butter Cups: $1.50
+  D Spearmint Gum: $0.25
+  E Charleston Chew: $1.00"
+end
 # ask the user how much money they have
 puts "Hello! Welcome to Charlotte's Candy Machine."
 puts "How much candy money do you have today?"
-x = gets.chomp.to_f
+print "$"
+candy_money = gets.chomp.to_f
+
+if candy_money.to_f <= 0.49
+  puts "You can't afford the good stuff.\n"
+  candy_money = gets.chomp.to_f
+end
 
 # display candy options and costs
 
-puts "I see you have #{x.to_f} to spend. Let's see what that gets you."
-puts "\nOur candy choices are below"
-puts "\nA $#{1.to_f} Twix"
-puts "\nB $#{0.75.to_f} M&Ms"
-puts "\nC $#{0.50.to_f} Reese's Peanut Buttercups"
-puts "\nD $#{0.25.to_f} A Small Pack of Spearmint Gum"
+puts "I see you have #{candy_money.to_f} to spend. Let's see what that gets you.\n\n"
+puts options
+puts "\n\n"
 
-A =1
-B =0.75
-C =0.50
-D =0.25
-
-puts "\nWhich candy would you like to select?"
-y=gets.chomp.to_f
+puts "\nSelect a letter to choose your candy.\n"
+candy_choice=gets.chomp.downcase
 
 
 
 # decide whether user can afford candy
 # if user can't afford, tell them so
-if x == y
-  print "You have just the right amount. Enjoy your sugar snack."
-elsif y == x
-  print "You have just the right amount. Enjoy your sugar snack."
-elsif x > y
-  print "Wow! You can definitely afford that!"
-  print "\nHere is your change.\n"
-   print x - y
-   
-# there's a bug in the code here that I haven't quite figured out, though I'm positive it's fairly rudimentary
-# when the total is calculated, it just repeats the value for x instead of solving the subtraction problem.
-# I'm not sure if it's where I assigned floats to variables, either. 
-
-elsif y > x
-  print "\nEnjoy your snack, and take your extra coins."
-  put "\nThis is your change"
-  print y - x 
-else
-  print "Take your business elsewhere, loser."
+if candy_choice == "a"
+  name = "Twix"
+  price = 0.75
+elsif candy_choice == "b"
+  name = "M and Ms"
+  price = 0.50
+elsif candy_choice == "c"
+  name = "Reese's Peanut Butter Cups" 
+  price = 1.50
+elsif candy_choice == "d"
+  name = Spearmint Gum
+  price = 0.25
+elsif candy_choice == "e"
+  name = "Charleston Chew"
+  price = 1.00
+elsif candy_choice != "a" && candy_choice != "b" && candy_choice != "c" && candy_choice != "d" && candy_choice != "e"
+  print "Take your business elsewhere, loser. Here's your money back."
 end
+
+if candy_money == price
+  puts "Here's your #{name}!"
+elsif candy_money < price
+  puts "This is not enough money, and I am not a charity."
+elsif candy_money > price
+  puts "Here's your #{name} and your change of #{candy_money-price.to_f}\n\n"
+end
+
+puts "Thank you for your business!"
